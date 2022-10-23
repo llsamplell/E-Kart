4a0654ac7656a0b932b3e1255eefb6b34bc71aec
Tanay Toshniwal <tanaytoshniwal98@gmail.com>
2022-10-24 00:11:48 +0530

    feat: initial FE changes

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import "antd/dist/antd.css";
+import React, { useLayoutEffect } from "react";
+import { Route, Switch, useLocation } from "react-router-dom";
+import Home from "./components/Home";
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Search from "./components/Search";
+import ipConfig from "./ipConfig.json";
+
+export const config = {
+  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+};
+
+export default function App(props) {
+  const location = useLocation();
+  // Scroll to top if path changes
+  useLayoutEffect(() => {
+    window && window.scrollTo(0, 0);
+  }, [location.pathname]);
+
+  return (
+    <div className="App">
+      <Switch>
+        <Route path="/register">
+          <Register />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/products">
+          <Search />
+        </Route>
+        <Route path="/hi">
+          <h1>Hi I'm here</h1>
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </div>
+  );
+}
